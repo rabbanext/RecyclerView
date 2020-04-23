fc56bd8b4e33de740b8c23555f2070c6cfa2a62d
R Muhammad Rabbani P <rabbaniarvian@gmail.com>
2020-04-23 19:15:38 +0700

    Cardview

---
+++ app/src/main/java/com/palimbanos/myrecyclerview/Hero.java
@@ -0,0 +1,40 @@
+package com.palimbanos.myrecyclerview;
+
+public class Hero {
+    private String name;
+    private String detail;
+    private String harga;
+    private int photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
