173991c27322208e9973aa798d722c880c44698b
R Muhammad Rabbani P <rabbaniarvian@gmail.com>
2020-04-24 14:13:39 +0700

    Commit

--- app/src/main/java/com/palimbanos/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/palimbanos/myrecyclerview/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -73,7 +74,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showAbout() {
-
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
     }
 
     @Override
